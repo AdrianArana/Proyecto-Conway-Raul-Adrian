464d76db916bf41aacd2ec592ad74ea4bef53ced
Adrián <adrianarana321@gmail.com>
2024-05-05 18:57:53 +0200

    COMMITEO

--- src/main/java/es/uah/matcomp/mped/proyectofinal/proyectoconwayrauladrian/HelloController.java
+++ src/main/java/es/uah/matcomp/mped/proyectofinal/proyectoconwayrauladrian/HelloController.java
@@ -1,19 +1,27 @@
 package es.uah.matcomp.mped.proyectofinal.proyectoconwayrauladrian;
 
-import javafx.event.ActionEvent;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
+
     @FXML
     private Label welcomeText;
+    private Stage scene;
+    public Button volverButton;
 
     @FXML
     protected void onHelloButtonClick() {
@@ -26,7 +34,7 @@ public class HelloController implements Initializable {
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("nombre.fxml"));
         try {
-            Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
             stage.setTitle("Nombre");
             stage.setScene(scene);
             stage.show();
@@ -34,12 +42,13 @@ public class HelloController implements Initializable {
             e.printStackTrace();
         }
     }
+
     @FXML
-    protected void onSiguienteButtonClick(){
+    protected void onSiguienteButtonClick() {
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("configuracion.fxml"));
-        try{
-            Scene scene=new Scene(fxmlLoader.load(),320,240);
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
             stage.setTitle("Configuración de parámetros");
             stage.setScene(scene);
             stage.show();
@@ -48,9 +57,28 @@ public class HelloController implements Initializable {
         }
     }
 
+    public void onCreditosButtonClick() {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("creditos.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+            stage.setTitle("Créditos");
+            stage.setScene(scene);
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    public void onVolverButtonClick(){
+        Stage stage=(Stage) volverButton.getScene().getWindow();
+        stage.close();
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
 }
--- src/main/java/es/uah/matcomp/mped/proyectofinal/proyectoconwayrauladrian/NombreController.java
+++ src/main/java/es/uah/matcomp/mped/proyectofinal/proyectoconwayrauladrian/NombreController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -12,19 +13,23 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NombreController implements Initializable {
+    public Button continuarButton;
 
     @FXML
-    protected void onSiguienteButtonClick(){
+    protected void onSiguienteButtonClick() {
+        Stage stageAnterior = (Stage) continuarButton.getScene().getWindow();
+        stageAnterior.close();
         Stage stage = new Stage();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("configuracion.fxml"));
-        try{
-            Scene scene=new Scene(fxmlLoader.load(),320,240);
+        try {
+            Scene scene = new Scene(fxmlLoader.load(), 600, 600);
             stage.setTitle("Configuración de parámetros");
             stage.setScene(scene);
-            stage.show();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 
 
